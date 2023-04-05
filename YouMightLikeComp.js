30961ea14994cebeafcc8dc59bb65c446efb3ddf
herb786 <herbert.caller@protonmail.com>
2023-04-05 02:07:55 -0500

    add carousel

--- YouMightLikeComp.js
+++ YouMightLikeComp.js
@@ -1,16 +1,23 @@
 export default {
     template: `
     <div class="resources">
-    <h3>You might also like</h3>
+    <h3>You may also like</h3>
     <ul>
-    <li><a href="https://spectrum.ieee.org/the-case-against-quantum-computing">The Case Against Quantum Computing</a></li>
-    <li><a href="https://blog.scottlogic.com/2023/03/13/beyond-the-hype-quantum-computing-part-one.html">
+    <li><a  target="_blank" href="https://spectrum.ieee.org/the-case-against-quantum-computing">The Case Against Quantum Computing</a></li>
+    <li><a  target="_blank" href="https://blog.scottlogic.com/2023/03/13/beyond-the-hype-quantum-computing-part-one.html">
             Beyond the Hype: Quantum Computing - hype and not hype simultaneously?</a></li>
-    <li><a href="https://rdm.mpdl.mpg.de/before-research/data-quality/">Data Quality</a></li>
-    <li><a href="https://www.ibm.com/topics/overfitting">What is overfitting?</a></li>
-    <li><a href="https://thealgorithmicbridge.substack.com/p/why-ai-is-doomed-without-neuroscience">Why AI Is Doomed Without Neuroscience</a></li>
-    <li><a href="https://www.techyourchance.com/kotlin-vs-java-whole-story/">Java vs Kotlin: Quantitative Analysis</a></li>
-    <li><a href="http://blog.cleancoder.com/uncle-bob/2016/07/27/TheChurn.html">The Clean Code Blog</a></li>
+    <li><a  target="_blank" href="https://rdm.mpdl.mpg.de/before-research/data-quality/">Data Quality</a></li>
+    <li><a  target="_blank" href="https://www.ibm.com/topics/overfitting">What is overfitting?</a></li>
+    <li><a  target="_blank" href="https://thealgorithmicbridge.substack.com/p/why-ai-is-doomed-without-neuroscience">Why AI Is Doomed Without Neuroscience</a></li>
+    <li><a  target="_blank" href="https://plato.stanford.edu/entries/chinese-room/">The Chinese Room Argument</a></li>
+    <li><a  target="_blank" href="https://jpt.spe.org/ai-delusion-why-humans-trump-machines">The AI Delusion: Why Humans Trump Machines</a></li>
+    <li><a  target="_blank" href="https://www.techyourchance.com/kotlin-vs-java-whole-story/">Java vs Kotlin: Quantitative Analysis</a></li>
+    <li><a  target="_blank" href="http://blog.cleancoder.com/uncle-bob/2016/07/27/TheChurn.html">The Clean Code Blog</a></li>
+    <li><a  target="_blank" href="https://docs.oracle.com/javase/tutorial/">The Java Tutorials</a></li>
+    <li><a  target="_blank" href="https://docs.swift.org/swift-book/documentation/the-swift-programming-language">The Swift Programming Language</a></li>
+    <li><a  target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">JavaScript</a></li>
+    <li><a  target="_blank" href="https://springframework.guru/gang-of-four-design-patterns/">Design Patterns</a></li>
+    <li><a  target="_blank" href="https://www.corporate-rebels.com/blog/stop-surfing-management-fads" >Stop Surfing The Management Fads</a></li>
     </ul>
     </div>
     `
---
+++ constants.js
@@ -0,0 +1,58 @@
+export const MY_APPS = [
+    {
+        "name": "ArxivLens",
+        "description": "ArXivLens has multiple features to look for preprints in ArXiv. The app includes most of the subjects and categories you find in ArXiv. You have a search history to reuse your old searches and find new results. The app opens preprints in pdf format using your installed external applications. The app lets you mail your search results to your colleagues or friends.",
+        "play": "https://play.google.com/store/apps/details?id=com.hacaller.arxivlens",
+        "itunes": "https://itunes.apple.com/us/app/arxivlens/id1352839831?mt=8",
+        "icono": "img/logo.png",
+        "fondo": "arxiv-bckg.png"
+    },
+    {
+        "name": "Fibromorse",
+        "description": "Provides common prosigns used in telegraphy. Provide an alphabet with morse code and respective sounds. Customize you app theme colour. Learn about the pioneers and inventors of the telegraph. A simple AR activity to show an old classic telegraph.",
+        "play": "https://play.google.com/store/apps/details?id=com.hacaller.fibromorse",
+        "itunes": "",
+        "icono": "img/logo2.png",
+        "fondo": "morse-bckg.png"
+    },
+    {
+        "name": "Andes Xchange",
+        "description": "Find daily quotes for popular currencies and cryptocurrencies.",
+        "play": "https://play.google.com/store/apps/details?id=com.hacaller.peruexchange&hl=es",
+        "itunes": "https://itunes.apple.com/us/app/andes-xchange/id1458186544",
+        "icono": "img/andesxchange.png",
+        "fondo": "common-bckg.png"
+    },
+    {
+        "name": "Citémosles!",
+        "description": "Share your quotes on your preferred social network like Twitter or Facebook in Spanish.",
+        "play": "",
+        "itunes": "https://itunes.apple.com/us/app/citémoslos/id1464253062",
+        "icono": "img/citemosles.png",
+        "fondo": "common-bckg.png"
+    },
+    {
+        "name": "QuoteThem!",
+        "description": "Share your quotes on your preferred social network like Twitter or Facebook.",
+        "play": "",
+        "itunes": "https://itunes.apple.com/us/app/quotethem/id1464253737",
+        "icono": "img/quotethem.png",
+        "fondo": "common-bckg.png"
+    },
+    {
+        "name": "Hangman Ninja",
+        "description": "Rescue the accused of his unfortunate fate with your knowledge. Do not let the ninja be near the gallows and perform the execution. You can choose different topics: Geography, Painters, Writers, Animals, Sports, Flowers, Trees, Minerals, Chemistry, Actors. Three levels of difficulty. You can play with 20, 30 or 40 words. At the moment only in spanish.",
+        "play": "https://play.google.com/store/apps/details?id=com.hacaller.theninjaexecutioner",
+        "itunes": "https://itunes.apple.com/us/app/hangman-ninja/id1449387511?ls=1&mt=8",
+        "icono": "img/hangman.png",
+        "fondo": "common-bckg.png"
+    },
+    {
+        "name": "Alien Sudoku",
+        "description": "A classical sudoku game with new levels.",
+        "play": "",
+        "itunes": "https://itunes.apple.com/us/app/sudoku-alien/id1450760182?ls=1&mt=8",
+        "icono": "img/sudoku.png",
+        "fondo": "common-bckg.png"
+    }
+]
