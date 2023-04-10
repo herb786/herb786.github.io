b691097e2017f962d7d82af3b33fddcb6054c97f
herb786 <herbert.caller@protonmail.com>
2023-04-10 18:51:16 -0500

    google dev page

--- IntroComp.js
+++ IntroComp.js
@@ -7,7 +7,7 @@ export default {
         <p>In this site you will find a short description of some apps I have published in the past years in different stores.</p>
         <p>You could also enjoy a few of my selected articles I have found on the interweb.</p></td>
         <td><a href="https://github.com/herb786"><img src="img/github-badge.png" /></a></td>
-        <td><a href="https://play.google.com/store/apps/developer?id=Hacagusae"><img src="img/google-play-badge.png" /></a></td>
+        <td><a href="https://play.google.com/store/apps/dev?id=6652170226309094323"><img src="img/google-play-badge.png" /></a></td>
         <td><a href="https://itunes.apple.com/us/developer/herbert-caller/id1352839830?mt=8"><img src="img/app-store-badge.png" /></a></td>
     </tr>
     </table>
