810b828d9ef71e171c0a305eab117214acd00a37
herb786 <herbert.caller@protonmail.com>
2023-04-11 18:21:33 -0500

    change title

--- HeaderComp.js
+++ HeaderComp.js
@@ -1,7 +1,7 @@
 export default {
     template: `
     <div class="header" id="header">
-        <div class="title">Heriberto Cagliari's Site</div>
+        <div class="title">Heriberto Casteddu's Site</div>
         <div class="owner"><span id="tagline">a.k.a Herbert Caller</span></div>
     </div>
     `
