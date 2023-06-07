f64d1eff5ca5ed6dd512c647cf687db186f5aaa8
herb786 <herbertcaller@HERB-MACMINI.local>
2023-06-06 22:51:21 -0500

    update doc links

--- YouMightLikeComp.js
+++ YouMightLikeComp.js
@@ -13,11 +13,12 @@ export default {
     <li><a  target="_blank" href="https://www.techyourchance.com/kotlin-vs-java-whole-story/">Java vs Kotlin: Quantitative Analysis</a></li>
     <li><a  target="_blank" href="http://blog.cleancoder.com/uncle-bob/2016/07/27/TheChurn.html">The Clean Code Blog</a></li>
     <li><a  target="_blank" href="http://www.slackware.com/">The Slackware Linux Project </a></li>
-    <li><a  target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/HTML">HTML: HyperText Markup Language</a></li>
+    <li><a  target="_blank" href="https://html.spec.whatwg.org/">HTML: HyperText Markup Language</a></li>
     <li><a  target="_blank" href="https://docs.oracle.com/javase/tutorial/">The Java Tutorials</a></li>
-    <li><a  target="_blank" href="https://docs.python.org/3/tutorial/index.html">The Python Tutorial¶</a></li>
+    <li><a  target="_blank" href="https://docs.python.org/3/tutorial/index.html">The Python Tutorial</a></li>
     <li><a  target="_blank" href="https://docs.swift.org/swift-book/documentation/the-swift-programming-language">The Swift Programming Language</a></li>
-    <li><a  target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">JavaScript</a></li>
+    <li><a  target="_blank" href="https://262.ecma-international.org/5.1/">JavaScript</a></li>
+    <li><a  target="_blank" href="https://www.typescriptlang.org/docs/">TypeScript</a></li>
     <li><a  target="_blank" href="https://springframework.guru/gang-of-four-design-patterns/">Design Patterns</a></li>
     <li><a  target="_blank" href="https://www.corporate-rebels.com/blog/stop-surfing-management-fads" >Stop Surfing The Management Fads</a></li>
     </ul>
