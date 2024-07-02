9baed3b05e860b23bcbb29e816bfa42fb5553ce0
Herbert Caller <herbertcaller@HERB-MACMINI.local>
2024-07-02 18:36:29 -0500

    websites

---
+++ WebComp.js
@@ -0,0 +1,15 @@
+export default {
+    template: `
+    <div class="paginas">
+        <h3>My Side Projects!</h3>
+        <div>
+            <a href="https://www.letrasfloridas.org/">LetrasFloridas.org</a>
+            <p>This website based on ReactJS has been created to change the font of short texts using the default unicode charset. This tool will allow you to change and edit the font of posts and descriptions without rectrictions.</p>
+        </div>
+        <div>
+            <a href="https://codice64.org/">Codice64.org</a>
+            <p>This website based on ReactJS provides a set of tools that could help developers to decode and encode using the protocol base64. You could also minify and beautify json an xml documents.</p>
+        </div>
+    </div>
+    `
+}
