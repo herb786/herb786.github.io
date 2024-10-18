bbad9fe48532c34e68ded4e0f9dea9fa5ecaa563
Herbert Caller <herbertcaller@HERB-MACMINI.local>
2024-10-17 21:06:18 -0500

    privacy app

--- FooterComp.js
+++ FooterComp.js
@@ -6,6 +6,7 @@ export default {
                 Copyright © 2024 Herbert Caller<br /><br />
                 <i class="fa fa-github-alt"></i> <a href="https://github.com/herb786">Github</a><br />
                 <i class="fa fa-legal"></i><a href="privacy.html">Privacy Policy</a><br />
+                <i class="fa fa-legal"></i><a href="privacy-app.html">App Privacy Policy</a><br />
                 <i class="fa fa-envelope"></i> <a href="mailto:spiffy.messenger@gmail.com">Contact</a>
             </div>
             <div class="right-footer">
