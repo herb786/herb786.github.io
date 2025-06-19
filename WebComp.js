8326ae40257fbf3cd685adef88e5c4e428cd14a1
HerbertCaller <herbert.caller@protonmail.com>
2025-06-19 17:58:40 -0500

    new site

--- WebComp.js
+++ WebComp.js
@@ -3,12 +3,8 @@ export default {
     <div class="paginas">
         <h3>My Side Projects!</h3>
         <div>
-            <a href="https://www.letrasfloridas.org/">LetrasFloridas.org</a>
+            <a href="https://conviertemisletras.com/">Conviertemisletras.com</a>
             <p>This website based on ReactJS has been created to change the font of short texts using the default unicode charset. This tool will allow you to change and edit the font of posts and descriptions without rectrictions.</p>
-        </div>
-        <div>
-            <a href="https://codice64.org/">Codice64.org</a>
-            <p>This website based on ReactJS provides a set of tools that could help developers to decode and encode using the protocol base64. You could also minify and beautify json an xml documents.</p>
         </div>
     </div>
     `
